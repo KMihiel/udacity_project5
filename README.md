c2130a0bc40be48090fef24f17a62a53567844d1
Kimberly Mihiel <kimberlymihiel@gmail.com>
2015-09-23 19:54:37 -0400

    Complete code rewrite

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
-<h3>Udacity Project 5: Neighborhood Map of Milford, Pennsylvania.</h3>
-<ol>
-	<li> Open Index.html in a web browser </li>
-	<li> The Map displays Milford,PA </li>
-	<li> Markers indicate locations of interest </li>
-	<li> Search for locations of interest in search bar </li>
-	<li> click items to view more information </li>
-</ol>
+#Project 5 - Neighborhood Map
 
-<h3>Steps to Create project:</h3>
-<ol>
-	<li>Download KnockOut.JS Framework</li>
-	<li>Include Bootstrap &amp; CSS </li>
-	<li>Add Google Maps API and Customize Data</li>
-	<li>Impliment Knockout.Js and infobox.js</li>
-</ol>
+link: http://tarheelsrule44.github.io/neighborhood-map/index.html
 
-<h3> Resources included: </h3>
-<ul>
-	<li>Knockout.Js</li>
-	<li>Google Maps API</li>
-	<li>infobox.js</li>
-</ul>
+This is a Single Page App that starts with a prepopulated list of resturants, bars, cafe's, etc. within Chapel Hill, NC.  
+
+The user can search for other places by keyword (food shops, fast food, etc.) or for a specific place.  
+
+The list and markers will update on search with information on all places within the search area. 
+
+Click on an item in the list to view more information from the Foursquare API such as Name, Phone Number, and Twitter handle. 
+
--- js/view.js
+++ js/view.js
@@ -1,281 +1,172 @@
-"use strict";
-//Start of models
-var model = {
-	//My chosen locations of interest for markers
-	myLocs : [
-	{
-		name : "Grey Towers",
-		lat : "41.322316",
-		lng : "-74.802388",
-		formatted_address : "122 Old Owego Turnpike Milford, PA 18337",
-		content : "Home of Gifford Pinchot, founder of the Forestry Movement",
-	},
-	{
-		name : "Kitatinny Canoes",
-		lat : "41.343152",
-		lng : "-74.759904",
-		formatted_address : "378 US-6 Milford, PA 18337",
-		content : "Go for a scenic boat trip down the Delaware River",
-	},
-	{
-		name : "Hotel Fauchere",
-		lat : "41.323639",
-		lng : "-74.801177",
-		formatted_address : "401 Broad St, Milford, PA 18337",
-		content : "The Best Hotel in Milford!",
-	},
-	{
-		name : "Waterwheel Cafe",
-		lat : "41.323920",
-		lng : "-74.809835",
-		formatted_address : "150 Water St Milford, PA 18337",
-		content : "Unique and Exciting Dining Experience",
-	},
-	{
-		name : "The Columns Museum",
-		lat : "41.326136",
-		lng : "-74.799191",
-		formatted_address : "608 Broad Street, Milford, PA 18337",
-		content : "A small town histroic museum featuring American artifacts.",
-	}
-	],
-	// initial marker for Milford PA
-	mapOptions : {
-    	zoom: 12,
-    	center: new google.maps.LatLng(41.3242,-74.8028)
-  	},
+function appViewModel() {
+  var self = this; 
+  var map;
+  var service;
+  var infowindow;
+  var lat = '';
+  var lng = '';
+//Lat and Lng of Milford Pennslyvania
+  var Milford = new google.maps.LatLng(41.3242,-74.8028);
+  var markersArray = [];  
+// array to hold info for knockout
+  self.allPlaces = ko.observableArray([]);
+// Finds the center of the map to get lat and lng values
+  function computeCenter() {
+    var latAndLng = map.getCenter();
+      lat = latAndLng.lat();
+      lng = latAndLng.lng(); 
+  };
 
-  	defaultBounds : new google.maps.LatLngBounds(
-			new google.maps.LatLng(41.36, -74.84),
-			new google.maps.LatLng(41.29, -74.74)),
-
-  	screenwidth : $(window).width(),
-
-	resultsWidth : $("#search-results").width(),
-
-	searchMarkers : []
-};
-
-var viewModel = {
-	getBounds : model.defaultBounds,
-
-	getMapOptions : this.mapOptions,
-
-	myLocs : model.myLocs,
-
-};
-
-var koViewModel = {
-	searchResults : ko.observableArray([]),
-	initialResults : ko.observableArray([]),
-	haveSearchResults : ko.observable(false),
-
-	clearResults : function() {
-		this.searchResults.removeAll();
-		//hide search results box
-		this.haveSearchResults(false);
-	},
-
-	resultClick : function(result) {
-		var address, setLat, setLon, searchLat, searchLon, request_url, lat, lon, streetWide, streetHeight;
-
-		address = result.formatted_address;
-		//Lat & lon from my points of interest
-		setLat = result.lat;
-		setLon = result.lng;
-		//Lat & Lon from searched locations
-		searchLat = (typeof result.geometry) === "undefined" ? "undefined" : result.geometry.location.A;
-		searchLon = (typeof result.geometry) === "undefined" ? "undefined" : result.geometry.location.F;
-		//Setting lat and lon
-		lat = searchLat !== "undefined" ? searchLat : setLat;
-		lon =  searchLon !== "undefined" ? searchLon : setLon;		
-	},
-
-	//click event for selecting markers on map
-	selectMarker : function(result){
-		var clickedMarker = model.searchMarkers[result.clickId];
-		google.maps.event.trigger(clickedMarker, 'click');
-	},
-};
-ko.applyBindings(koViewModel);
+//Loads Map, Search & List Locations
+  function initialize() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: Milford,    
+    });
+    getPlaces();
+    computeCenter();       
+//Shows list of locations with markers
+    var list = (document.getElementById('list'));
+    map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(list);
+    var input = (document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(
+      (input));
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+      clearOverlays();
+      self.allPlaces.removeAll();
+      var bounds = new google.maps.LatLngBounds();  
 
 
-var mapView = {
-	init : function () {
-		//Map location
-		var markers = [];
-	  	var map = new google.maps.Map(document.getElementById('map-canvas'), viewModel.getMapOptions);
+      for(var i=0, place; i<10; i++){
+        if (places[i] !== undefined){
+          place = places[i];
 
-		//Search box start
-		var defaultBounds = viewModel.getBounds;
-		map.fitBounds(defaultBounds);
+          getAllPlaces(place);
+          createMarker(place);
+          bounds.extend(place.geometry.location);          
+        };       
+      };
+      map.fitBounds(bounds); 
+      computeCenter();                
+    });
+    google.maps.event.addListener(map, 'bounds_changed', function(){
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });      
+  };
 
-		// Create the search box and link it to the UI element.
-		var input = /** @type {HTMLInputElement} */(
-		  document.getElementById('search-input'));
-		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// Function to show markers on the map with place types created by google
+  function getPlaces() {
+    var request = {
+      location: Milford,
+      radius: 600,
+      types: ['museum','library','bakery', 'bar']
+    };
 
-		var searchBox = new google.maps.places.SearchBox(
-		(input));
+    infowindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);    
+  };
 
-		// find info related to search item
-		google.maps.event.addListener(searchBox, 'places_changed', function() {
-			model.searchMarkers = [];
-			var places = searchBox.getPlaces();
+//Creates markers for lcoations
+  function callback(results, status){
+    if (status == google.maps.places.PlacesServiceStatus.OK){
+      bounds = new google.maps.LatLngBounds();
+      results.forEach(function (place){
+        place.marker = createMarker(place);
+        bounds.extend(new google.maps.LatLng(
+          place.geometry.location.lat(),
+          place.geometry.location.lng()));
+      });
+      map.fitBounds(bounds);
+      results.forEach(getAllPlaces);                 
+    };
+  };
 
 
+//Function to create a marker at each place.
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      name: place.name.toLowerCase(),
+      position: place.geometry.location,
+      place_id: place.place_id,
+  // Added brief animation where markers drop onto map
+      animation: google.maps.Animation.DROP
+    });    
+    var address;
+    if (place.vicinity !== undefined) {
+      address = place.vicinity;
+    } else if (place.formatted_address !== undefined) {
+      address = place.formatted_address;
+    };     
+    var contentString = '<div>' + place.name + '</div><div>' + address + '</div>';
 
-			var placesLength = places.length;
+    google.maps.event.addListener(marker, 'click', function() {      
+      infowindow.setContent(contentString);      
+      infowindow.open(map, this);
+      map.panTo(marker.position); 
+  // Added brief bounce to markers when clicked on map
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){marker.setAnimation(null);}, 1450);
+    });
 
-			for(var i = 0; i < placesLength; i++){
-				places[i].clickId = [i];
-				koViewModel.searchResults.push(places[i]);
+    markersArray.push(marker);
+    return marker;
+  };
+   
+  //Change position to center over active marker
+  self.clickMarker = function(place) {
+    var marker;
 
-				koViewModel.initialResults(koViewModel.searchResults.slice(0));
-			}
+    for(var e = 0; e < markersArray.length; e++) {      
+      if(place.place_id === markersArray[e].place_id) { 
+        marker = markersArray[e];
+      // Added brief bounce to markers when clicked in list
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function(){marker.setAnimation(null);}, 1450);
+        break; 
+      };
+    } ;        
+    map.panTo(marker.position);        
+  };
 
-			koViewModel.haveSearchResults(true);
-			$("#left-info").removeClass("results-hide");
 
-			if (placesLength === 0) {
-				return;
-			}
+  /*
+  function that gets the information from all the places that we are going to search and also pre-populate.  Pushes this info to the allPlaces array for knockout.
+  */
+  function getAllPlaces(place){
+    var myPlace = {};    
+    myPlace.place_id = place.place_id;
+    myPlace.position = place.geometry.location.toString();
+    myPlace.name = place.name;
 
-			for (var i = 0; i < markers.length; i++) {
-				marker.setMap(null);
-			}
+    var address;    
+    if (place.vicinity !== undefined) {
+      address = place.vicinity;
+    } else if (place.formatted_address !== undefined) {
+      address = place.formatted_address;
+    }
+    myPlace.address = address;
+    
+    self.allPlaces.push(myPlace);                
+  }
 
-			var bounds = new google.maps.LatLngBounds();
-			markers = [];
 
-			for (var i = 0, place; place = places[i]; i++) {
-				var image = {
-			    	url: place.icon,
-			    	size: new google.maps.Size(71, 71),
-			    	origin: new google.maps.Point(0, 0),
-			    	anchor: new google.maps.Point(17, 34),
-			    	scaledSize: new google.maps.Size(25, 25)
-			  	};
+  /*
+  called after a search, this function clears any markers in the markersArray so that we can start with fresh map with new markers.
+  */
+  function clearOverlays() {
+    for (var i = 0; i < markersArray.length; i++ ) {
+     markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
+  } 
 
-				// Create a marker point for each location.
-				var marker = new google.maps.Marker({
-				    map: map,
-				    icon: image,
-				    name: place.name,
-				    position: place.geometry.location,
-				    id: [i]
-				});
+  google.maps.event.addDomListener(window, 'load', initialize);
+}
 
-				markers.push(marker);
-				model.searchMarkers = markers;
-
-				//Custom infobox with class for css and id for javascript
-				var boxText = '<div id="infobox" class="infobox-outer"><div class="infobox-inner">';
-	        		boxText += place.name + "<br/>" + place.formatted_address;
-	        		boxText += '</div></div>';
-
-				var infoboxOptions = {
-					content : boxText,
-					disableAutoPan: false,
-					maxWidth: 0,
-					pixelOffset: new google.maps.Size(-145, -10),
-					zIndex: null,
-					boxStyle: {
-						opacity: 1,
-						width: "280px",
-					},
-					closeBoxMargin: "10px 2px 2px 2px",
-					infoBoxClearance: new google.maps.Size(1, 1),
-					isHidden: false,
-					pane: "floatPane"
-				};
-
-				//Add listener for marker click events
-				google.maps.event.addListener(marker, 'click',(function(marker, i) {
-
-					var infobox = new InfoBox(infoboxOptions);
-
-					return function(){
-						//Remove old infobox if any
-						$('#infobox').remove();
-						//Add new infobox for new item
-						infobox.open(map, marker);
-						koViewModel.resultClick(places[i]);
-						//Center the map on chosen location
-						var latLng = marker.getPosition();
-						map.setCenter(latLng);
-					};
-				})(marker, i));
-
-				bounds.extend(place.geometry.location);
-			}
-
-		map.fitBounds(bounds);
-		});
-
-		google.maps.event.addListener(map, 'bounds_changed', function() {
-			var bounds = map.getBounds();
-			searchBox.setBounds(bounds);
-		});
-		mapView.setMarkers(map, viewModel.myLocs);
-	},
-
-	//Add my points of interest markers to the map when the page originally loads 
-	setMarkers : function(map, locations) {
-
-		var myLocLength = viewModel.myLocs.length;
-
-		for(var i = 0; i < myLocLength; i++){
-
-			//Create map marker 
-			var marker = new google.maps.Marker({
-		    	position: new google.maps.LatLng( viewModel.myLocs[i].lat , viewModel.myLocs[i].lng ),
-		    	map: map,
-		    	name: viewModel.myLocs[i].name
-			});
-
-			//Custom infobox for styling 
-	        var boxText = '<div id="infobox" class="infobox-outer"><div class="infobox-inner">';
-	        	boxText += viewModel.myLocs[i].name + "<br/>" + viewModel.myLocs[i].formatted_address + "<br/>" + viewModel.myLocs[i].content;
-	        	boxText += '</div></div>';
-
-			var infoboxOptions = {
-		            content : boxText,
-		            disableAutoPan: false,
-		            maxWidth: 0,
-		            pixelOffset: new google.maps.Size(-140, 0),
-		            zIndex: null,
-		            boxStyle: {
-                		opacity: 1,
-                		width: "280px",
-            		},
-		 			closeBoxMargin: "10px 2px 2px 2px",
-		 			infoBoxClearance: new google.maps.Size(1, 1),
-		 			isHidden: false,
-		 			pane: "floatPane"
-		        };
-
-			//Add event listener for marker click events
-			google.maps.event.addListener(marker, 'click', (function(marker, i) {
-				//Create marker info window
-				var infobox = new InfoBox(infoboxOptions);
-
-				return function(){
-					//Remove old infobox if any
-					$('#infobox').remove();
-					//Add new infobox
-					infobox.open(map, marker);
-					//Un-hide results window and display results
-					koViewModel.resultClick(viewModel.myLocs[i]);
-					$("#left-info").removeClass("results-hide");
-					//Center the map on clicked marker
-					var latLng = marker.getPosition();
-					map.setCenter(latLng);
-					console.log(marker);
-				};
-
-			})(marker, i));
-		}
-	}
-};
-google.maps.event.addDomListener(window, 'load', mapView.init);
+$(function(){
+ko.applyBindings(new appViewModel());
+});
